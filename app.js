b696af3c5eb005584dc6080ac35c8f4d8eba8081
saipranay305 <saipranay305@gmail.com>
2025-07-21 20:46:51 +0530

    Basic Styling using Bootstrap

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs=require("ejs");
 const Listing=require("./models/listing");
 const path=require("path")
 const methodOverride=require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 main().then(()=>{
@@ -22,6 +23,8 @@ app.set("views",path.join(__dirname,"views"));
 
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public"))); 
 app.get("/",(req,res)=>{
     res.send("Home Page!")
 })
